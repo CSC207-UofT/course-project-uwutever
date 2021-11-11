00ea1d624a6e63d9e9af5f56beae61f2ae182fde
JerryFan <hanrui.fan@mail.utoronto.ca>
2021-11-11 18:45:02 -0500

    Unfinished Change to MainActivity for recyclerview and cardview and ADD new RegexCard Class and Adapter(Unfinished).

---
+++ src/com/terraincognita/Android/RegexApp/app/src/main/java/com/example/regex/RegexCard.java
@@ -0,0 +1,5 @@
+package com.example.regex;
+
+public class RegexCard {
+    private String title;
+}
---
+++ src/com/terraincognita/Android/RegexApp/app/src/main/java/com/example/regex/RegexCardAdapter.java
@@ -0,0 +1,32 @@
+package com.example.regex;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+class RegexCardAdapter extends RecyclerView.Adapter {
+
+    private ArrayList<RegexCard> mRegexData;
+    private Context mContext;
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
