e2f5f964388891a8d9fbeaf1f1989ae9edb1288a
bbrianh <brianhomanchon@gmail.com>
2021-11-12 01:34:04 -0500

    Added getAcceptingStates, getStates, delta methods in FSA abstract class

--- src/com/terraincognita/automata/FSA.java
+++ src/com/terraincognita/automata/FSA.java
@@ -2,6 +2,8 @@ package com.terraincognita.automata;
 
 import com.terraincognita.automata.states.FSAState;
 
+import java.util.Collection;
+
 public abstract class FSA <T extends FSAState>{
     /**
      * Return the start state of the FSA
@@ -9,7 +11,25 @@ public abstract class FSA <T extends FSAState>{
     public abstract T getStartState();
 
     /**
-     * Run the FSA from the start state with a given string
+     * Return the set of final states of the FSA
+     */
+    public abstract Collection<T> getAcceptingStates();
+
+    /**
+     * Return the set of states of the FSA
+     */
+    public abstract Collection<T> getStates();
+
+    /**
+     * The delta function of the FSA
+     * Transition function of a given state and alphabet
+     * @param fromState the state where the transition starts
+     * @param alphabet the alphabet for the transition
+     */
+    public abstract Object delta(T fromState, String alphabet);
+
+    /**
+     * Run the FSA from the start state with a given string (delta* function)
      * @param alphabets a given string to run the FSA
      * @return the reached state(s)
      */
