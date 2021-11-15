1ccfd381aa6ffd99a2a6d124f0fc2f94ad609789
Arkaprava Choudhury <arkaprava.choudhury@outlook.com>
2021-11-15 11:36:41 -0500

    add some unit tests

--- src/test/TestNFA.java
+++ src/test/TestNFA.java
@@ -1,4 +1,51 @@
 package test;
 
+import com.terraincognita.lexer.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class TestNFA {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    /*
+    Test whether the NFA is initialized properly
+    This unit test is responsible for testing multiple aspects of the NFA class, viz. getStates(), getAlphabets(),
+    getStartStates()
+     */
+    @Test
+    public void testNFAConstruction() {
+
+    }
+
+    /*
+    Test whether the NFA for a single character is initialized properly, when using the `String` constructor
+     */
+    @Test
+    public void testNFAChar1() {
+
+    }
+
+    /*
+    Test whether the NFA for a single character is initialized properly, when using the `NFA` constructor
+     */
+    @Test
+    public void testNFAChar2() {
+
+    }
+
+    @After
+    public void throwDown() throws Exception {
+
+    }
 }
