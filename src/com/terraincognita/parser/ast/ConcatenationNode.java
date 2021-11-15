31d72e560c4e314dd8e117d730e51827fefc3f1d
kevingaojx <gaojunxuanbox@live.com>
2021-11-14 23:42:56 -0600

    [U] Add comments; slight modification to ASTNode that should not affect existing codes

--- src/com/terraincognita/parser/ast/ConcatenationNode.java
+++ src/com/terraincognita/parser/ast/ConcatenationNode.java
@@ -6,9 +6,17 @@ import java.util.ArrayList;
 
 public class ConcatenationNode extends ASTNode {
 
-    public ConcatenationNode() {
+    /**
+     * An <code>ASTNode</code> representing the concatenation operation.
+     *
+     * This node has children, which are stored in <code>left</code> and <code>right</code>,
+     * respectively.
+     */
+    public ConcatenationNode(ASTNode left, ASTNode right) {
         super();
         this.operator = Token.createConcat();
+        this.left = left;
+        this.right = right;
     }
 
     @Override
--- src/com/terraincognita/parser/ast/ZeroOrOneNode.java
+++ src/com/terraincognita/parser/ast/ZeroOrOneNode.java
@@ -6,9 +6,15 @@ import java.util.ArrayList;
 
 public class ZeroOrOneNode extends ASTNode {
 
-    public ZeroOrOneNode() {
+    /**
+     * An <code>ASTNode</code> representing the '?' operation that matches a given character zero or one time.
+     *
+     * This node has only one child, which is stored in the <code>exp</code> field.
+     */
+    public ZeroOrOneNode(ASTNode exp) {
         super();
         this.operator = Token.createToken('?');
+        this.exp = exp;
     }
 
     @Override
