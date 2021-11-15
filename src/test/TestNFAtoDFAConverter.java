06d32924945d971cf61ff70725729817812f8ad2
bbrianh <brianhomanchon@gmail.com>
2021-11-15 13:39:28 -0500

    Created subset construction tests

--- src/test/TestNFAtoDFAConverter.java
+++ src/test/TestNFAtoDFAConverter.java
@@ -5,3 +5,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 import org.junit.Assert.*;
+
+public class TestNFAtoDFAConverter {
+    /*
+    Set up a sample NFA with a few states
+    Convert the NFA to DFA
+     */
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    /*
+    compare the states
+     */
+    @Test
+    public void compareStates(){
+
+    }
+
+    /*
+    compare the start states
+     */
+    @Test
+    public void compareStartState(){
+
+    }
+
+    /*
+    compare the start states
+     */
+    @Test
+    public void comapreAcceptingStates(){
+
+    }
+
+    /*
+    compare the transitions
+     */
+    @Test
+    public void compareTransitions(){}
+
+}
