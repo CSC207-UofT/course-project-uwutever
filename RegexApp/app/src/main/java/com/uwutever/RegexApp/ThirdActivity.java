ca400a9ac6e29863a49561b28ac672bb689b2c94
JerryFan <hanrui.fan@mail.utoronto.ca>
2021-12-05 21:31:32 -0500

    Add pixel for Graph Example

--- RegexApp/app/src/main/java/com/uwutever/RegexApp/ThirdActivity.java
+++ RegexApp/app/src/main/java/com/uwutever/RegexApp/ThirdActivity.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.uwutever.RegexApp.utils.controllers.MatchController;
-
 import net.xqhs.graphs.graph.Node;
 import net.xqhs.graphs.graph.SimpleEdge;
 import net.xqhs.graphs.graph.SimpleNode;
@@ -83,24 +81,24 @@ public class ThirdActivity extends AppCompatActivity {
 
         Node v1 = new SimpleNode("18");
         Node v2 = new SimpleNode("24");
-        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.png)));
-        graph.getVertex().add(new Vertex(v2, ContextCompat.getDrawable(this, R.drawable.png)));
+        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.smallpixel)));
+        graph.getVertex().add(new Vertex(v2, ContextCompat.getDrawable(this, R.drawable.smallpixel)));
         graph.addEdge(new SimpleEdge(v1, v2, "12"));
 
         Node v3 = new SimpleNode("7");
-        graph.getVertex().add(new Vertex(v3, ContextCompat.getDrawable(this, R.drawable.png)));
+        graph.getVertex().add(new Vertex(v3, ContextCompat.getDrawable(this, R.drawable.smallpixel)));
         graph.addEdge(new SimpleEdge(v2, v3, "23"));
 
         v1 = new SimpleNode("14");
-        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.png)));
+        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.smallpixel)));
         graph.addEdge(new SimpleEdge(v3, v1, "34"));
 
         v1 = new SimpleNode("10");
-        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.png)));
+        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.smallpixel)));
         graph.addEdge(new SimpleEdge(v3, v1, "35"));
 
         v1 = new SimpleNode("11");
-        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.png)));
+        graph.getVertex().add(new Vertex(v1, ContextCompat.getDrawable(this, R.drawable.smallpixel)));
         graph.addEdge(new SimpleEdge(v1, v3, "36"));
         graph.addEdge(new SimpleEdge(v3, v1, "6"));
 
