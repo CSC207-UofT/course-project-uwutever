0ef529d7782ea8799d251b615257e1e716f252cb
bbrianh <brianhomanchon@gmail.com>
2021-12-06 01:31:34 -0500

    fixed searcher algorithm

--- RegexApp/app/src/main/java/com/uwutever/RegexApp/utils/automata/Searcher.java
+++ RegexApp/app/src/main/java/com/uwutever/RegexApp/utils/automata/Searcher.java
@@ -15,24 +15,19 @@ public class Searcher {
 
         // loop for every char in text as the starting char
         for(int start = 0; start < text.length(); start++){
-            int end = start + 1;
-
-            // if match is found, get the longest match and append index pair
-            if (fsa.accept(text.substring(start, end))){
-                //get the longest substring
-                while(fsa.accept(text.substring(start, end + 1))){
-                    end += 1;
+            for(int end = start + 1; end <= text.length(); end ++){
+                // if match is found, get the longest match and append index pair
+                if (fsa.accept(text.substring(start, end))){
+                    if(end==text.length() || !fsa.accept(text.substring(start, end+1))){
+                        //append index pair
+                        List<Integer> indexPair = new ArrayList<>();
+                        indexPair.add(start);
+                        indexPair.add(end);
+                        ret.add(indexPair);
+                    }
                 }
-
-                //append index pair
-                List<Integer> indexPair = new ArrayList<>();
-                indexPair.add(start);
-                indexPair.add(end);
-                ret.add(indexPair);
             }
         }
-
         return ret;
     }
-
 }
